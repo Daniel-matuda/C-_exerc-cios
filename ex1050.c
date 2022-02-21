7a565984d650058edd8703a7fd2d619ee2cd5468
Daniel Guilherme Sousa Matuda <99701602+Daniel-matuda@users.noreply.github.com>
2022-02-20 23:44:49 -0300

    Add files via upload

---
+++ ex1050.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main (){
+    int ddd;
+    printf("Digite o DDD:");
+    scanf("%d",&ddd);
+
+    if (ddd == 61)
+        printf("Brasilia\n");
+        else if (ddd == 71)
+            printf("Salvador\n");
+            else if (ddd == 11)
+                printf("Sao Paulo");
+                else if (ddd == 21)
+                    printf("Rio de Janeiro");
+                    else if (ddd == 32)
+                        printf("Juiz de Fora");  
+                        else if (ddd == 19)
+                            printf("Campinas");
+                            else if (ddd == 27)
+                                printf("Vitoria");
+                                else if (ddd == 31)
+                                    printf("Belo Horizonte");
+                                    else printf ("DDD nao cadastrado");
+
+}
